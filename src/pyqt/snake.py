0891d56631bc02e7bb875787ca0c772d6b9ac74b
xiaoliuzi <qingpengliu@qq.com>
2016-07-04 12:43:13 +0800

    change to brush

--- src/pyqt/snake.py
+++ src/pyqt/snake.py
@@ -18,21 +18,42 @@ class Snake(QtGui.QWidget):
     def paintEvent(self, event):
         qp = QtGui.QPainter()
         qp.begin(self)
-        self.drawPoints(qp)
+        #self.drawPoints(qp)
+        for i in range (10, 200, 50):
+            self.drawRectangles(qp, i)
+            timer = QtCore.QTimer()
+            timer.start(2000)
+            self.update()
         qp.end()
 
         #self.paintBorderEvent(QtCore.Qt.Key_B)
         #self.paintSnakeEvent(QtCore.Qt.Key_B)
         #self.paintFood(QtCore.Qt.Key_B)
+    
+    def drawRectangles(self, qp, i):
+        color = QtGui.QColor(0, 0, 0)
+        color.setNamedColor("#d4d4d4")
+        qp.setPen(color)
+
+        qp.setBrush(QtGui.QColor(200-i, 0, 0))
+        qp.drawRect(10, 15, 90, 60)
+
+        qp.setBrush(QtGui.QColor(255-i, 80, 0, 160))
+        qp.drawRect(130, 15, 90, 60)
+
+        qp.setBrush(QtGui.QColor(25+i, 0, 90, 200))
+        qp.drawRect(250, 15, 90, 60)
+
+
 
     def drawPoints(self, qp):
-        qp.setPen(QtCore.Qt.green)
-        sieze = self.size()
+        qp.setPen(QtCore.Qt.red)
+        size = self.size()
         
         for i in range(1000):
             x = random.randint(1, size.width()-1)
             y = random.randint(1, size.height()-1)
-            qp.drawPoint(x, y)
+            #qp.drawPoint(x, y)
 
 
     def paintBorderEvent(self, event):
@@ -49,10 +70,12 @@ class Snake(QtGui.QWidget):
         pborn_snake.begin(self)
 
         pborn_snake.setPen(QColor(Qt.red))
+        """
         for j in range(0, 100):
             for i in range(20, 40, 10):
                 pborn_snake.drawPoint(i+10, 170)
-
+        """
+        
         pborn_snake.end()
 
     #---draw food
@@ -64,24 +87,27 @@ class Snake(QtGui.QWidget):
 
         pfood.end()
 
-    def SnakeMove(steps):
+    def SnakeMove(self):
         #paintSnake(self, e)
         print 'hello '
+#        self.xlz_move()
+"""
+    def xlz_move(self):
+        pborn_snake.begin()
+        pborn_snake.setpen(QColor(Qt.black))
+        pborn_snake.drawPoint(100, 170)
+        pborn_snake.drawPoint(110, 170)
+        pborn_snake.end()
+"""
 
 def main():
     
     app = QApplication(sys.argv)
-    
     s = Snake()
- 
-"""
-    timer = QtCore.QTimer()
-    timer.timeout.connect(s.SnakeMove)
-    timer.start(2000)
 
-    timer.singleShot(60000, app.quit)
-    #s.paintBorder(self, e )
-"""
+#    timer = QtCore.QTimer()
+#    timer.timeout.connect(s.paintEvent )
+#    timer.start(2000)
 
     sys.exit(app.exec_())
 
