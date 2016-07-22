27a5d78b1969231db6a9751023166d379c83fca1
xiaoliuzi <qingpengliu@qq.com>
2016-07-22 12:40:47 +0800

    timer

---
+++ src/pyqt/timer06/Timer.py
@@ -0,0 +1,14 @@
+import threading
+import time
+
+def hello(name):
+    print "hello %s\n" %name
+
+    global timer
+    timer = threading.Timer(2.0, hello, ["Hawk"])
+    timer.start()
+
+if __name__ == "__main__":
+    timer = threading.Timer(2.0, hello, ["Hawk"])
+    timer.start()
+
